c0a8dd65dbae837186fa8db8458a1c38330486a8
Lionel Koenig <lionel.koenig@gmail.com>
2020-06-16 23:29:14 +0200

    Some stuff

--- src/index.js
+++ src/index.js
@@ -1,62 +1,71 @@
 "use strict";
 const GEOLOCATION_OPTIONS = {
-    enableHighAccuracy: true,
-    timeout: 0,
-    maximumAge: 0
+  enableHighAccuracy: true,
+  timeout: 0,
+  maximumAge: 0
 };
 
-let latitude = document.getElementById('latitude');
-let longitude = document.getElementById('longitude');
-
 const formatDegree = function (degree) {
-    let d = Math.floor(degree);
-    let m = (degree - d);
-    return `${d.toString().padStart(3, "0")}°${(m * 60).toFixed(3)}`;
+  let d = Math.floor(degree);
+  let m = (degree - d);
+  return `${d}°${(m * 60).toFixed(3)}`;
 };
 
 const formatLatitude = function (latitude) {
-    if (latitude > 0) {
-        return `N ${formatDegree(latitude)}`;
-    } else {
-        return `S ${formatDegree(-latitude)}`;
-    }
+  if (latitude > 0) {
+    return `N ${formatDegree(latitude)}`;
+  } else {
+    return `S ${formatDegree(-latitude)}`;
+  }
 };
 
 const formatLongitude = function (longitude) {
-    if (longitude > 0) {
-        return "E " + formatDegree(longitude);
-    } else {
-        return "W " + formatDegree(-longitude);
-    }
+  if (longitude > 0) {
+    return "E " + formatDegree(longitude);
+  } else {
+    return "W " + formatDegree(-longitude);
+  }
 };
 
+const formatTime = function (time) {
+  return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()} ${time.getHours().toString().padStart(2, "0")}:${time.getMinutes().toString().padStart(2, "0")}:${time.getSeconds().toString().padStart(2, "0")}`
+}
+
 class GpsApp {
-    constructor() {
-        this.watch_id = null;
+  constructor() {
+    this.latitude = document.getElementById('latitude');
+    this.longitude = document.getElementById('longitude');
+    this.time = document.getElementById('time');
+
+    this.watch_id = null;
+
+    this.lastKnownPosition = null;
+    this.manOverBoardPosition = null;
+  }
+
+  start() {
+    this.watch_id = navigator.geolocation.watchPosition(
+      function (position) {
+        this.lastKnownPosition = position;
+        this.latitude.innerHTML = formatLatitude(position.coords.latitude);
+        this.longitude.innerHTML = formatLongitude(position.coords.longitude);
+        this.time.innerHTML = formatTime(new Date(position.timestamp));
+      }.bind(this),
+      function (err) {
+        console.log(err);
+      },
+      GEOLOCATION_OPTIONS
+    );
+  };
+
+  stop() {
+    if (this.watch_id != null) {
+      navigator.geolocation.clearWatch(this.watch_id);
     }
-
-    Start() {
-        this.watch_id = navigator.geolocation.watchPosition(
-            function (position) {
-                console.log(position);
-                latitude.innerHTML = formatLatitude(position.coords.latitude);
-                longitude.innerHTML = formatLongitude(position.coords.longitude);
-            },
-            function (err) {
-                console.log(err);
-            },
-            GEOLOCATION_OPTIONS
-        );
-    };
-
-    Stop() {
-        if (this.watch_id != null) {
-            navigator.geolocation.clearWatch(this.watch_id);
-        }
-    };
+  };
 
 };
 
 
 const app = new GpsApp();
-app.Start();
+app.start();
